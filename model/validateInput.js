fc3196f2fd35daee3649e9d570235b562b46a663
Osvaldo Mesa <d3v.ozzy@gmail.com>
2024-07-14 14:41:43 -0700

    update validation to check for input type

--- model/validateInput.js
+++ model/validateInput.js
@@ -1,35 +1,53 @@
+const checkType = (day, month, year, obj) => {
+	const genericMessage = 'Must be a valid '
+	let pass = true
+
+	if (day !== 0 && !day) {
+		obj.day = genericMessage + 'day'
+		pass = false
+	}
+	if (month !== 0 && !month) {
+		obj.month = genericMessage + 'month'
+		pass = false
+	}
+	if (year !== 0 && !year) {
+		obj.year = genericMessage + 'year'
+		pass = false
+	}
+	if (!pass) obj.pass = false
+
+	return pass
+}
 const validateInput = (day, month, year) => {
 	console.log('validateInput arguments day, month, year:', day, month, year)
 	const date = new Date(year, month - 1, day)
-	// function returnObj(target) {
-	// 	return {
-	// 		target, message: `Must be a valid ${target}`
-	// 	}
-	// }
-	const returnObj = {
-		day: 'Pass',
-		month: 'Pass',
-		year: 'Pass',
+	const testObj = {
+		pass: true,
+		day: 'pass',
+		month: 'pass',
+		year: 'pass',
 	}
 
+	if (!checkType(day, month, year, testObj)) return testObj
+
 	if (year > new Date().getFullYear()) {
-		returnObj.year = 'Must be in the past'
+		testObj.year = 'Must be in the past'
 	}
 	if (year.toString().length < 4) {
-		returnObj.year = 'Please enter a year in YYYY format'
+		testObj.year = 'Please enter a year in YYYY format'
 	}
-	// if (date.getFullYear() !== year) {
-	// 	returnObj.year = 'Must be a valid year'
-	// }
+	if (year < 0) {
+		testObj.year = 'Must be a valid year'
+	}
 	if (month < 1 || month > 12) {
-		returnObj.month = 'Must be a valid month'
+		testObj.month = 'Must be a valid month'
 	}
-	// if (date.getMonth() !== month - 1) {
-	// 	returnObj.month = 'Must be a valid month'
-	// }
 	if (date.getDate() !== day) {
-		returnObj.day = 'Must be a valid day'
+		testObj.day = 'Must be a valid day'
 	}
-	return returnObj
+	if (testObj.day !== 'pass' || testObj.month !== 'pass' || testObj.year !== 'pass') {
+		testObj.pass = false
+	}
+	return testObj
 }
 export default validateInput
