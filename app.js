97e4bd574a145eb0a1525edd9bd0f8ba57e4c05c
Osvaldo Mesa <d3v.ozzy@gmail.com>
2024-07-13 17:32:54 -0700

    init git

---
+++ app.js
@@ -0,0 +1,9 @@
+import express from 'express'
+import HomePage from './components/HomePage.js'
+
+const app = express()
+app.listen(3000, () => console.log('Listening on port 3000'))
+
+app.get('/', (req, res) => {
+	res.send(HomePage())
+})
