e00ef631c5b5881d9868f09e892ef5432a7d857e
Osvaldo Mesa <d3v.ozzy@gmail.com>
2024-07-13 19:15:12 -0700

    create htmx routes to handle UI update

--- app.js
+++ app.js
@@ -4,6 +4,22 @@ import HomePage from './components/HomePage.js'
 const app = express()
 app.listen(3000, () => console.log('Listening on port 3000'))
 
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }));
+
 app.get('/', (req, res) => {
 	res.send(HomePage())
+})
+
+app.post('/input', (req, res) => {
+	//	TODO: need to update logic to validate input before replacing display card
+	const day = req.body.DD
+	const month = req.body.MM
+	const year = req.body.YYYY
+	console.log(req.body)
+	res.send(`
+<div>${year} years</div>
+<div>${month} months</div>
+<div>${day} days</div>
+`)
 })
