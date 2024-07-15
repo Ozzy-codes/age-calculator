5551ba1087a21aaf6048bf9158575281b0868265
Osvaldo Mesa <d3v.ozzy@gmail.com>
2024-07-15 00:41:48 -0700

    consolidate check past logic - bug found
    Separating validation checks for all inputs leads to
    input fields passing other tests prematurely.

--- model/validateInput.js
+++ model/validateInput.js
@@ -18,6 +18,25 @@ const checkType = (day, month, year, obj) => {
 
 	return pass
 }
+
+const checkPastDate = (day, month, year, obj) => {
+	let pass = true
+	if (year > new Date().getFullYear()) {
+		obj.year = 'Must be in the past'
+		pass = false
+	}
+	if (month > new Date().getMonth() + 1) {
+		obj.month = 'Must be in the past'
+		pass = false
+	}
+	if (day > new Date().getDate()) {
+		obj.day = 'Must be in the past'
+		pass = false
+	}
+	if (!pass) obj.pass = false
+	return pass
+}
+
 const validateInput = (day, month, year) => {
 	console.log('validateInput arguments day, month, year:', day, month, year)
 	const date = new Date(year, month - 1, day)
@@ -29,22 +48,23 @@ const validateInput = (day, month, year) => {
 	}
 
 	if (!checkType(day, month, year, testObj)) return testObj
+	if (!checkPastDate(day, month, year, testObj)) return testObj
 
-	if (year > new Date().getFullYear()) {
-		testObj.year = 'Must be in the past'
-	}
 	if (year.toString().length < 4) {
 		testObj.year = 'Please enter a year in YYYY format'
 	}
 	if (year < 0) {
 		testObj.year = 'Must be a valid year'
 	}
+
 	if (month < 1 || month > 12) {
 		testObj.month = 'Must be a valid month'
 	}
+
 	if (date.getDate() !== day) {
 		testObj.day = 'Must be a valid day'
 	}
+
 	if (testObj.day !== 'pass' || testObj.month !== 'pass' || testObj.year !== 'pass') {
 		testObj.pass = false
 	}
