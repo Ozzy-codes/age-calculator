763f464f47db785de5deb783d409157fa22e066d
Osvaldo Mesa <d3v.ozzy@gmail.com>
2024-07-18 22:31:25 -0700

    update variable name, remove logs, handle additional case in getAge fn

--- model/getAge.js
+++ model/getAge.js
@@ -1,6 +1,6 @@
 const getAge = (birthDay, birthMonth, birthYear) => {
 	const birthDayInt = +birthDay
-	const birthMonthInt = +birthMonth - 1
+	const birthMonthIdx = +birthMonth - 1
 	const birthYearInt = +birthYear
 
 	const currentDate = new Date()
@@ -8,17 +8,14 @@ const getAge = (birthDay, birthMonth, birthYear) => {
 	const currMonth = currentDate.getMonth()
 	const currDay = currentDate.getDate()
 
-	const monthDiff = birthMonthInt - currMonth
-	console.log('monthDiff: ', monthDiff)
+	const monthDiff = birthMonthIdx - currMonth
 	const dayDiff = birthDayInt - currDay
-	console.log('dayDiff: ', dayDiff)
 
 	let calculatedYear;
 	let calculatedMonth;
 	let calculatedDay;
 
 	if (monthDiff === 0) {
-		console.log('monthdiff is equal to zero')
 		if (dayDiff >= 0) {
 			calculatedYear = currYear - birthYearInt
 			calculatedMonth = 0
@@ -28,29 +25,29 @@ const getAge = (birthDay, birthMonth, birthYear) => {
 			calculatedMonth = 11
 			calculatedDay = (new Date(currYear, currMonth + 1, 0).getDate()) - currDay + birthDayInt
 		}
-		console.log('output year, month, day: ', calculatedYear, calculatedMonth, calculatedDay)
 		return { calculatedYear, calculatedMonth, calculatedDay }
 	} else if (monthDiff > 0) {
 		calculatedYear = currYear - birthYearInt - 1
-		calculatedMonth = 12 - birthMonthInt + currMonth - 1
+		calculatedMonth = 11 - birthMonthIdx + currMonth
 		if (dayDiff === 0) {
 			calculatedMonth += 1
 			calculatedDay = 0
 		} else if (dayDiff > 0) {
-			calculatedDay = birthDayInt - currDay
+			calculatedDay = (new Date(currYear, currMonth, 0).getDate()) - birthDayInt + currDay
 		} else {
-			calculatedDay = (new Date(currYear, currMonth + 1, 0).getDate()) - currDay + birthDayInt
+			calculatedDay = currDay - birthDayInt
+			calculatedMonth += 1
 		}
 		return { calculatedYear, calculatedMonth, calculatedDay }
 	} else if (monthDiff < 0) {
 		calculatedYear = currYear - birthYearInt
-		calculatedMonth = currMonth - birthMonthInt
+		calculatedMonth = currMonth - birthMonthIdx
 		if (dayDiff === 0) {
 			calculatedDay = 0
 		} else if (dayDiff < 0) {
 			calculatedDay = currDay - birthDayInt
 		} else {
-			calculatedDay = (new Date(currYear, birthMonthInt + 1, 0).getDate()) - birthDayInt + currDay
+			calculatedDay = (new Date(currYear, birthMonthIdx + 1, 0).getDate()) - birthDayInt + currDay
 			calculatedMonth -= 1
 		}
 		return { calculatedYear, calculatedMonth, calculatedDay }
