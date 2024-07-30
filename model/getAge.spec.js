7a7c8b233f7ca3ec15ae7222ac53563ff073fe85
Osvaldo Mesa <d3v.ozzy@gmail.com>
2024-07-30 14:45:12 -0700

    add test - birthday and month equal to todays day and month

--- model/getAge.spec.js
+++ model/getAge.spec.js
@@ -1,6 +1,17 @@
 import { it, expect } from "vitest"
 import getAge from "./getAge"
 
+it('should get correct age if birth month is = to the current month and the birth day of the month is the same day as today', () => {
+	const day = new Date().getDate()
+	const month = ((new Date().getMonth() + 1) % 13) || 1
+	const year = new Date().getFullYear() - 1
+
+	const output = getAge(day, month, year)
+
+	expect(output.calculatedDay).toBe(0)
+	expect(output.calculatedMonth).toBe(0)
+	expect(output.calculatedYear).toBe(1)
+})
 it('should get correct age if birth month is > than current month and the birth day of the month is the same day as today', () => {
 	const day = new Date().getDate()
 	const month = ((new Date().getMonth() + 6) % 13) || 1
